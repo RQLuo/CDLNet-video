5037397d850373aa61de2d3096307b0d88c1c2a5
罗仁卿 Renqing Luo <103999523+RQLuo@users.noreply.github.com>
2024-10-16 02:16:05 -0400

    Add files via upload

--- model/net.py
+++ model/net.py
@@ -241,6 +241,7 @@ def prox_CSR(u, z_prev, lambd, gamma):
         torch.Tensor: Result after applying the proximal operator.
     """
     return ST(ST(u - z_prev - lambd * torch.sign(z_prev), lambd * gamma) + z_prev + lambd * torch.sign(z_prev), lambd)
+
 def prox_CSR_f2(u, z_prev, z_after, lambd, gamma1, gamma2):
     """
     Proximal operator of the CSR penalty using soft-thresholding.
@@ -248,13 +249,18 @@ def prox_CSR_f2(u, z_prev, z_after, lambd, gamma1, gamma2):
     Parameters:
         u (torch.Tensor): Current tensor.
         z_prev (torch.Tensor): Previous frame's tensor.
+        z_after (torch.Tensor): Afters frame's tensor.
         lambd (torch.Tensor): Outer threshold.
-        gamma (torch.Tensor): Inner threshold.
-
+        gamma1 (torch.Tensor): Inner threshold.
+        gamma2 (torch.Tensor): Midder threshold.
     Returns:
         torch.Tensor: Result after applying the proximal operator.
     """
-    return ST(ST(u - z_prev - lambd * torch.sign(z_prev), gamma) + z_prev + lambd * torch.sign(z_prev), lambd)
+    Ca = z_prev + lambd * torch.sign(z_prev) + lambd * gamma2 * torch.sign(z_prev - z_after)
+    Cb = z_after + lambd * torch.sign(z_after) + lambd * gamma1 * torch.sign(z_after - z_prev)
+    inner = ST(u - Ca, gamma1*lambd)
+    midder = ST(inner - Cb + lambd * gamma1 * torch.sign(u - Ca), gamma2*lambd)
+    return ST(midder + Cb - lambd * gamma1 * torch.sign(u - Ca), lambd)
 
 class CDLNet_CSR(nn.Module):
     """ Convolutional Dictionary Learning Network:
@@ -355,7 +361,111 @@ class CDLNet_CSR(nn.Module):
         xphat = self.D(z)
         xhat = post_process(xphat, params)
         return xhat, z
+class CDLNet_CSRf2(nn.Module):
+    """ Convolutional Dictionary Learning Network:
+    Interpretable denoising DNN with adaptive thresholds for robustness, using prox_CSR_f2.
+    """
+    def __init__(self,
+                 K=3,            # num. unrollings
+                 M=64,           # num. filters in each filter bank operation
+                 P=7,            # square filter side length
+                 s=1,            # stride of convolutions
+                 C=1,            # num. input channels
+                 t0=0,           # initial threshold
+                 adaptive=False, # noise-adaptive thresholds
+                 init=True):     # False -> use power-method for weight init
+        super(CDLNet_CSRf2, self).__init__()
+        
+        # -- OPERATOR INIT --
+        self.A = nn.ModuleList([nn.Conv2d(C, M, P, stride=s, padding=(P-1)//2, bias=False) for _ in range(K)])
+        self.B = nn.ModuleList([nn.ConvTranspose2d(M, C, P, stride=s, padding=(P-1)//2, output_padding=s-1, bias=False) for _ in range(K)])
+        self.D = self.B[0]  # alias D to B[0], otherwise unused as z0 is zero
+        self.t = nn.Parameter(t0 * torch.ones(K, 2, M, 1, 1))  # learned thresholds
+        self.g1 = nn.Parameter(t0 * torch.ones(K, 2, M, 1, 1)) # learned inner thresholds
+        self.g2 = nn.Parameter(t0 * torch.ones(K, 2, M, 1, 1)) # learned middle thresholds
+        # set weights 
+        W = torch.randn(M, C, P, P)
+        for k in range(K):
+            self.A[k].weight.data = W.clone()
+            self.B[k].weight.data = W.clone()
+        
+        # Don't bother running code if initializing trained model from state-dict
+        if init:
+            print("Running power-method on initial dictionary...")
+            with torch.no_grad():
+                DDt = lambda x: self.D(self.A[0](x))
+                L = power_method(DDt, torch.rand(1, C, 128, 128), num_iter=200, verbose=False)[0]
+                print(f"Done. L={L:.3e}.")
+                
+                if L < 0:
+                    print("STOP: something is very very wrong...")
+                    sys.exit()
+            
+            # spectral normalization (note: D is aliased to B[0])
+            for k in range(K):
+                self.A[k].weight.data /= np.sqrt(L)
+                self.B[k].weight.data /= np.sqrt(L)
+        
+        # set parameters
+        self.K = K
+        self.M = M
+        self.P = P
+        self.s = s
+        self.t0 = t0
+        self.adaptive = adaptive
 
+    @torch.no_grad()
+    def project(self):
+        """ \ell_2 ball projection for filters, R_+ projection for thresholds """
+        self.t.clamp_(0.0) 
+        for k in range(self.K):
+            self.A[k].weight.data = uball_project(self.A[k].weight.data)
+            self.B[k].weight.data = uball_project(self.B[k].weight.data)
+
+    def forward(self, y, z_prev=None, z_after=None, sigma=None, mask=1):
+        """
+        LISTA + D with temporal information for 3D denoising, utilizing prox_CSR_f2.
+
+        Parameters:
+            y (torch.Tensor): Noisy input.
+            z_prev (torch.Tensor, optional): Previous frame's sparse code. Defaults to None.
+            z_after (torch.Tensor, optional): After frame's sparse code. Defaults to None.
+            sigma (float, optional): Noise level. Defaults to None.
+            mask (torch.Tensor, optional): Mask for convolutions. Defaults to 1.
+
+        Returns:
+            tuple: (Denoised output, Current sparse code)
+        """ 
+        yp, params, mask = pre_process(y, self.s, mask=mask)
+
+        # THRESHOLD SCALE-FACTOR c
+        c = 0 if sigma is None or not self.adaptive else sigma / 255.0
+
+        if z_prev is None and z_after is not None:
+            z = prox_CSR(self.A[0](yp), z_after, self.t[0, :1] + c * self.t[0, 1:2], self.g2[0, :1] + c * self.g2[0, 1:2])
+        elif z_after is None and z_prev is not None:
+            z = prox_CSR(self.A[0](yp), z_prev, self.t[0, :1] + c * self.t[0, 1:2], self.g1[0, :1] + c * self.g1[0, 1:2])
+        elif z_after is not None and z_prev is not None:
+            z = prox_CSR_f2(self.A[0](yp), z_prev, z_after, self.t[0, :1] + c * self.t[0, 1:2], 
+                           self.g1[0, :1] + c * self.g1[0, 1:2], self.g2[0, :1] + c * self.g2[0, 1:2])
+        else:
+            z = ST(self.A[0](yp), self.t[0, :1] + c * self.t[0, 1:2])
+
+        for k in range(1, self.K):
+            u = z - self.A[k](mask * self.B[k](z) - yp)
+            if z_prev is None and z_after is not None:
+                z = prox_CSR(u, z_after, self.t[k, :1] + c * self.t[k, 1:2], self.g2[k, :1] + c * self.g2[k, 1:2])
+            elif z_after is None and z_prev is not None:
+                z = prox_CSR(u, z_prev, self.t[k, :1] + c * self.t[k, 1:2], self.g1[k, :1] + c * self.g1[k, 1:2])
+            elif z_after is not None and z_prev is not None:
+                z = prox_CSR_f2(u, z_prev, z_after, self.t[k, :1] + c * self.t[k, 1:2], 
+                            self.g1[k, :1] + c * self.g1[k, 1:2], self.g2[k, :1] + c * self.g2[k, 1:2])
+            else:
+                z = ST(u, self.t[k, :1] + c * self.t[k, 1:2])
+        # DICTIONARY SYNTHESIS
+        xphat = self.D(z)
+        xhat = post_process(xphat, params)
+        return xhat, z
 class GDLNet(nn.Module):
     """ Gabor Dictionary Learning Network:
     """
