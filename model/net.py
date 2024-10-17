656a8cf70c96b432c2fcb88a6496ec19dc0f5098
罗仁卿 Renqing Luo <103999523+RQLuo@users.noreply.github.com>
2024-10-17 17:57:34 -0400

    Add files via upload

--- model/net.py
+++ model/net.py
@@ -33,8 +33,7 @@ class CDLNet(nn.Module):
         self.B = nn.ModuleList([nn.ConvTranspose2d(M, C, P, stride=s, padding=(P-1)//2, output_padding=s-1, bias=False) for _ in range(K)])
         self.D = self.B[0]                              # alias D to B[0], otherwise unused as z0 is zero
         self.t = nn.Parameter(t0*torch.ones(K,2,M,1,1)) # 1st num layer, 2nd t_0 + t_1 _sighat, 3rd channel dim, 4th and 5th boardcast
-        self.g = nn.Parameter(t0*torch.ones(K,2,M,1,1))
-        # set weights 
+        self.g = nn.Parameter(t0 * torch.ones(K,2,M,1,1))
         W = torch.randn(M, C, P, P)
         for k in range(K):
             self.A[k].weight.data = W.clone()
@@ -262,7 +261,7 @@ def prox_CSR_f2(u, z_prev, z_after, lambd, gamma1, gamma2):
     midder = ST(inner - Cb + lambd * gamma1 * torch.sign(u - Ca), gamma2*lambd)
     return ST(midder + Cb - lambd * gamma1 * torch.sign(u - Ca), lambd)
 
-class CDLNet_CSR(nn.Module):
+class CDLNet_CSR_old(nn.Module):
     """ Convolutional Dictionary Learning Network:
     Interpretable denoising DNN with adaptive thresholds for robustness.
     """
@@ -349,13 +348,114 @@ class CDLNet_CSR(nn.Module):
 
         # Iterative updates
         for k in range(1, self.K):
-            # Gradient descent step
-            u = z - self.A[k](mask * self.B[k](z) - yp)
             if z_prev is not None:
+                u = z - self.A[k](mask * self.B[k](z) - yp)
                 z = prox_CSR(u, z_prev, self.t[k, :1] + c * self.t[k, 1:2], self.g[k, :1] + c * self.g[k, 1:2])
             else:
-                # Spatial proximal operator without temporal information
+                u = z - self.A[k](mask * self.B[k](z) - yp)
                 z = ST(u, self.t[k, :1] + c * self.t[k, 1:2])
+
+        # DICTIONARY SYNTHESIS
+        xphat = self.D(z)
+        xhat = post_process(xphat, params)
+        return xhat, z
+
+class CDLNet_CSR(nn.Module):
+    """ Convolutional Dictionary Learning Network:
+    Interpretable denoising DNN with adaptive thresholds for robustness.
+    """
+    def __init__(self,
+                 K=3,            # num. unrollings
+                 M=64,           # num. filters in each filter bank operation
+                 P=7,            # square filter side length
+                 s=1,            # stride of convolutions
+                 C=1,            # num. input channels
+                 t0=0,           # initial threshold
+                 adaptive=False, # noise-adaptive thresholds
+                 init=True):     # False -> use power-method for weight init
+        super(CDLNet_CSR, self).__init__()
+        
+        # -- OPERATOR INIT --
+        self.A = nn.ModuleList([nn.Conv2d(C, M, P, stride=s, padding=(P-1)//2, bias=False) for _ in range(K)])
+        self.B = nn.ModuleList([nn.ConvTranspose2d(M, C, P, stride=s, padding=(P-1)//2, output_padding=s-1, bias=False) for _ in range(K)])
+        self.A2 = nn.ModuleList([nn.Conv2d(C, M, P, stride=s, padding=(P-1)//2, bias=False) for _ in range(K)])
+        self.B2 = nn.ModuleList([nn.ConvTranspose2d(M, C, P, stride=s, padding=(P-1)//2, output_padding=s-1, bias=False) for _ in range(K)])
+        self.D = self.B[0]  # alias D to B[0], otherwise unused as z0 is zero
+        self.t = nn.Parameter(t0 * torch.ones(K, 2, M, 1, 1))  # learned thresholds
+        self.t2 = nn.Parameter(t0 * torch.ones(K, 2, M, 1, 1))  # learned thresholds
+        self.g = nn.Parameter(t0 * torch.ones(K,2,M,1,1))
+        # set weights 
+        W = torch.randn(M, C, P, P)
+        for k in range(K):
+            self.A[k].weight.data = W.clone()
+            self.B[k].weight.data = W.clone()
+        
+        # Don't bother running code if initializing trained model from state-dict
+        if init:
+            print("Running power-method on initial dictionary...")
+            with torch.no_grad():
+                DDt = lambda x: self.D(self.A[0](x))
+                L = power_method(DDt, torch.rand(1, C, 128, 128), num_iter=200, verbose=False)[0]
+                print(f"Done. L={L:.3e}.")
+                
+                if L < 0:
+                    print("STOP: something is very very wrong...")
+                    sys.exit()
+            
+            # spectral normalization (note: D is aliased to B[0])
+            for k in range(K):
+                self.A[k].weight.data /= np.sqrt(L)
+                self.B[k].weight.data /= np.sqrt(L)
+        
+        # set parameters
+        self.K = K
+        self.M = M
+        self.P = P
+        self.s = s
+        self.t0 = t0
+        self.adaptive = adaptive
+
+    @torch.no_grad()
+    def project(self):
+        """ \ell_2 ball projection for filters, R_+ projection for thresholds """
+        self.t.clamp_(0.0) 
+        for k in range(self.K):
+            self.A[k].weight.data = uball_project(self.A[k].weight.data)
+            self.B[k].weight.data = uball_project(self.B[k].weight.data)
+
+    def forward(self, y, z_prev=None, sigma=None, mask=1):
+        """
+        LISTA + D with temporal information for 3D denoising.
+
+        Parameters:
+            y (torch.Tensor): Noisy input.
+            z_prev (torch.Tensor, optional): Previous frame's sparse code. Defaults to None.
+            sigma (float, optional): Noise level. Defaults to None.
+            mask (torch.Tensor, optional): Mask for convolutions. Defaults to 1.
+
+        Returns:
+            tuple: (Denoised output, Current sparse code)
+        """ 
+        yp, params, mask = pre_process(y, self.s, mask=mask)
+
+        # THRESHOLD SCALE-FACTOR c
+        c = 0 if sigma is None or not self.adaptive else sigma / 255.0
+
+        # Initialize z
+        if z_prev is None:
+            z = ST(self.A2[0](yp), self.t2[0, :1] + c * self.t2[0, 1:2])
+        else:
+            # Incorporate temporal information using Prox_CSR for the first iteration if needed
+            z = prox_CSR(self.A[0](yp), z_prev, self.t[0, :1] + c * self.t[0, 1:2], self.g[0, :1] + c * self.g[0, 1:2])
+
+        # Iterative updates
+        for k in range(1, self.K):
+            if z_prev is not None:
+                u = z - self.A[k](mask * self.B[k](z) - yp)
+                z = prox_CSR(u, z_prev, self.t[k, :1] + c * self.t[k, 1:2], self.g[k, :1] + c * self.g[k, 1:2])
+            else:
+                u = z - self.A2[k](mask * self.B2[k](z) - yp)
+                z = ST(u, self.t2[k, :1] + c * self.t2[k, 1:2])
 
         # DICTIONARY SYNTHESIS
         xphat = self.D(z)
